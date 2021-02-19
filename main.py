f996a433591c448cad5b10775bb03abea26369ce
Sha2020-ship-it <72983931+Sha2020-ship-it@users.noreply.github.com>
2021-02-19 13:16:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import smtplib
+from datetime import datetime as dt
 import pickle
 
 FOCUS_POINTS = 0
@@ -36,36 +37,41 @@ def emails():
         s.sendmail("math.pi.study@gmail.com", f'{i}', message)
 
 
-
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
 eyes_detector = cv2.CascadeClassifier('haarascade_eye.xml')
 
 webcam = cv2.VideoCapture(0)
-while True:
-    successful_frame_read, frame = webcam.read()
+while 1:
 
-    if not successful_frame_read:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 7) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 3):
+        successful_frame_read, frame = webcam.read()
+
+        if not successful_frame_read:
+            break
+
+        frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(frame_grayscale)
+        smiles = smile_detector.detectMultiScale(frame_grayscale)
+
+        for (x, y, w, h) in faces:
+            the_face = frame[y: y + h, x: x + w]
+            face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
+            smiles = smile_detector.detectMultiScale(face_grayscale, 1.4, 20)
+            if len(smiles) > 0:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
+                FOCUS_POINTS -= 10
+            else:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
+                FOCUS_POINTS += 10
+
+        cv2.imshow('Smile Detector', frame)
+        cv2.waitKey(1)
+    else:
         break
-
-    frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(frame_grayscale)
-    smiles = smile_detector.detectMultiScale(frame_grayscale)
-
-    for (x, y, w, h) in faces:
-        the_face = frame[y: y + h, x: x + w]
-        face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
-        smiles = smile_detector.detectMultiScale(face_grayscale, 1.4, 20)
-        if len(smiles) > 0:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
-            FOCUS_POINTS -= 10
-        else:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
-            FOCUS_POINTS += 10
-
-    cv2.imshow('Smile Detector', frame)
-    cv2.waitKey(1)
 
 webcam.release()
 cv2.destroyAllWindows()
 emails()
+
+print(f'Your focus score: {FOCUS_POINTS}')
