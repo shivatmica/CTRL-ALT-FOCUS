1863338072a93d05ead54433ba98311a3d8229ca
Sha2020-ship-it <72983931+Sha2020-ship-it@users.noreply.github.com>
2021-02-19 18:44:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import cv2
-import smtplib
 import pickle
-import numpy as np
 
 RED_TIMES = 0
 GREEN_TIMES = 0
@@ -12,16 +10,17 @@ with open(filepath) as file:
     content = file.readlines()
 
 outfile = open('password.pkl', 'wb')
-pickle.dump(content ,outfile)
+pickle.dump(content, outfile)
 outfile.close()
 state = None
 
+face_detector = cv2.CascadeClassifier('C:\\Users\\sharv\\PycharmProjects\\OPENCV\\haarcascade_frontalface_default.xml')
+smile_detector = cv2.CascadeClassifier('C:\\Users\\sharv\\PycharmProjects\\OPENCV\\haarcascade_smile.xml')
+eyes_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
-eyes_detector = cv2.CascadeClassifier('haarascade_eye.xml')
 count = 0
 webcam = cv2.VideoCapture(0)
+
 while 1:
     successful_frame_read, frame = webcam.read()
 
@@ -31,26 +30,31 @@ while 1:
     frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(frame_grayscale)
     smiles = smile_detector.detectMultiScale(frame_grayscale)
+    eyes = eyes_detector.detectMultiScale(frame_grayscale)
 
     for (x, y, w, h) in faces:
         the_face = frame[y: y + h, x: x + w]
         face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
-        smiles = smile_detector.detectMultiScale(face_grayscale, 1.4, 20)
+        smiles = smile_detector.detectMultiScale(face_grayscale, 1.8, 20)
+        eyes = eyes_detector.detectMultiScale(face_grayscale, 1.8, 20)
         if len(smiles) > 0:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
             RED_TIMES += 1
         else:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
             GREEN_TIMES += 1
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(frame, (ex, ey), (ex + ew, ey + eh), (0, 0, 0), 4)
 
     cv2.imshow('Smile Detector', frame)
     cv2.waitKey(1)
 
     count += 1
-    if count == 500:
+    if count == 100:
         webcam.release()
         cv2.destroyAllWindows()
 
 
-print(f'No. of times you were focues: {GREEN_TIMES // 10}')
-print(f'No. of times you were distracted {RED_TIMES // 10}')
+print(f'No. of times you were focused: {GREEN_TIMES // 5}')
+print(f'No. of times you were distracted {RED_TIMES // 5}')
+print(f'Percentage of focus = {round(((GREEN_TIMES / (GREEN_TIMES + RED_TIMES)) * 100))}%')
