9a41f94e57110bc6c799024e6d0c2b4da12df8f7
Sha2020-ship-it <72983931+Sha2020-ship-it@users.noreply.github.com>
2021-02-19 22:48:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Imports and Inits
 import cv2
 import smtplib
+import ssl
 import pickle
 
 # Password for the email and saving the password in a database
@@ -19,45 +20,57 @@ pickle.dump(content, outfile)
 outfile.close()
 state = None
 
+print('Only google email accepted.')
+parent = input("Enter your parent's email: ")
+password = content[0]
+
+
 # Function to end emails to anyone
 def emails():
-    print('Only google email accepted.')
-    parent = input("Enter your parent's email: ")
-    password = content[0]
-    
     # Starting the SMTP session with port 587 - gmail
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-    
-    s.login("math.pi.study@gmail.com", content[0])
-    
+    global state
+    port = 587  # For starttls
+    smtp_server = "smtp.gmail.com"
+
+    sender_email = "focusmeter330@gmail.com"
+    receiver_email = parent
+    pw = content[0]
+
     # Determining the state of the child's focus
     if round(((GREEN_TIMES / (GREEN_TIMES + RED_TIMES)) * 100)) in range(50, 76):
         state = "Your child was quite focused during this time."
-        
+
     elif round(((GREEN_TIMES / (GREEN_TIMES + RED_TIMES)) * 100)) in range(75, 101):
         state = "Your child was very focused during this time."
-        
+
     elif round(((GREEN_TIMES / (GREEN_TIMES + RED_TIMES)) * 100)) in range(25, 51):
         state = "Your child was not very focused during this time."
-        
+
     elif round(((GREEN_TIMES / (GREEN_TIMES + RED_TIMES)) * 100)) in range(0, 26):
         state = "Your child was not at all focused during this time."
 
-   # Making the content of the message
+    # Making the content of the message
     message = f"""No. of times you were focused: {GREEN_TIMES // 5}.  
                   No. of times you were distracted {RED_TIMES // 5}. 
                   Percentage of focus = {round(((GREEN_TIMES / (GREEN_TIMES + RED_TIMES)) * 100))}%. 
                   {state}"""
-    
-    s.sendmail("math.pi.study@gmail.com", parent, message)
-    s.quit()
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, pw)
+        server.sendmail(sender_email, receiver_email, message)
+
 
 # .XML files and haarcascades to detect the faces and smile
 face_detector = cv2.CascadeClassifier('C:\\Users\\sharv\\PycharmProjects\\OPENCV\\haarcascade_frontalface_default.xml')
 
 smile_detector = cv2.CascadeClassifier('C:\\Users\\sharv\\PycharmProjects\\OPENCV\\haarcascade_smile.xml')
 
+eye_detector = cv2.CascadeClassifier('C:\\Users\\sharv\\PycharmProjects\\OPENCV\\haarascade_eye.xml')
 count = 0
 
 webcam = cv2.VideoCapture(0)
@@ -70,25 +83,24 @@ while 1:
 
     frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(frame_grayscale)
-    
+
     smiles = smile_detector.detectMultiScale(frame_grayscale)
 
     for (x, y, w, h) in faces:
         the_face = frame[y: y + h, x: x + w]
         face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
-        
-        # Detecting the smiles
+
+        # Detecting the smiles and eyes
         smiles = smile_detector.detectMultiScale(face_grayscale, 1.8, 20)
-        
+
         # Making the green or red rectangle according to the smile
         if len(smiles) > 0:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
             RED_TIMES += 1
-        
+
         else:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
             GREEN_TIMES += 1
-        
 
     cv2.imshow('Smile Detector', frame)
     cv2.waitKey(1)
