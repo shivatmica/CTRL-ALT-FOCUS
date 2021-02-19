e1c038eb573eac65875929ac122e405641d1b116
Sha2020-ship-it <72983931+Sha2020-ship-it@users.noreply.github.com>
2021-02-19 17:01:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cv2
 import smtplib
-from datetime import datetime as dt
 import pickle
+import numpy as np
 
-FOCUS_POINTS = 0
+RED_TIMES = 0
+GREEN_TIMES = 0
 filepath = "C:\\Users\\sharv\\password.txt"
 
 with open(filepath) as file:
@@ -15,63 +16,41 @@ pickle.dump(content ,outfile)
 outfile.close()
 state = None
 
-print('Only gmail emails will be accepted.')
-cord = input('Enter the email of your coordinator of school: ')
-parent = input('Enter the email of your parent: ')
-emails = [cord, parent]
-
-def emails():
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-    s.login("math.pi.study@gmail.com", f"{content}")
-    if FOCUS_POINTS == 0:
-        state = 'OK'
-    elif FOCUS_POINTS > 0:
-        state = 'Good'
-    else:
-        state = 'Bad'
-    message = """The number of points your child had throughout the day was {}. 
-                 Our rating for him today is {}""".format(FOCUS_POINTS, state)
-
-    for i in emails:
-        s.sendmail("math.pi.study@gmail.com", f'{i}', message)
-
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
 eyes_detector = cv2.CascadeClassifier('haarascade_eye.xml')
-
+count = 0
 webcam = cv2.VideoCapture(0)
 while 1:
+    successful_frame_read, frame = webcam.read()
 
-    if dt(dt.now().year, dt.now().month, dt.now().day, 7) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 3):
-        successful_frame_read, frame = webcam.read()
-
-        if not successful_frame_read:
-            break
-
-        frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_detector.detectMultiScale(frame_grayscale)
-        smiles = smile_detector.detectMultiScale(frame_grayscale)
-
-        for (x, y, w, h) in faces:
-            the_face = frame[y: y + h, x: x + w]
-            face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
-            smiles = smile_detector.detectMultiScale(face_grayscale, 1.4, 20)
-            if len(smiles) > 0:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
-                FOCUS_POINTS -= 10
-            else:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
-                FOCUS_POINTS += 10
-
-        cv2.imshow('Smile Detector', frame)
-        cv2.waitKey(1)
-    else:
+    if not successful_frame_read:
         break
 
-webcam.release()
-cv2.destroyAllWindows()
-emails()
+    frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(frame_grayscale)
+    smiles = smile_detector.detectMultiScale(frame_grayscale)
 
-print(f'Your focus score: {FOCUS_POINTS}')
+    for (x, y, w, h) in faces:
+        the_face = frame[y: y + h, x: x + w]
+        face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
+        smiles = smile_detector.detectMultiScale(face_grayscale, 1.4, 20)
+        if len(smiles) > 0:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
+            RED_TIMES += 1
+        else:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
+            GREEN_TIMES += 1
+
+    cv2.imshow('Smile Detector', frame)
+    cv2.waitKey(1)
+
+    count += 1
+    if count == 500:
+        webcam.release()
+        cv2.destroyAllWindows()
+
+
+print(f'No. of times you were focues: {GREEN_TIMES // 10}')
+print(f'No. of times you were distracted {RED_TIMES // 10}')
