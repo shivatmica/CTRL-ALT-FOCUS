f408a43887b5ffa55d4d1a805fe2b4a05be008a7
Sha2020-ship-it <72983931+Sha2020-ship-it@users.noreply.github.com>
2021-02-19 09:24:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import cv2
-
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-video_capture = cv2.VideoCapture(0)
-
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
+webcam = cv2.VideoCapture(0)
 while True:
-    ret, frame = video_capture.read()
+    successful_frame_read, frame = webcam.read()
+    if not successful_frame_read:
+        break
+    frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(frame_grayscale)
+    smiles = smile_detector.detectMultiScale(frame_grayscale, 1.4, 20)
+    for (x, y, w, h) in faces :
+        the_face = frame[y:y+h, x:x+w]
+        face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
+        smiles = smile_detector.detectMultiScale(face_grayscale, 1.4, 20)
+        if len(smiles) > 0:
+            cv2.rectangle(frame,(x, y), (x+w, y+h), (0, 0, 255), 4)
+        else:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    cv2.imshow('Video', frame)
+    cv2.imshow('Smile Detector', frame)
     cv2.waitKey(1)
-
-video_capture.release()
+webcam.release()
 cv2.destroyAllWindows()
