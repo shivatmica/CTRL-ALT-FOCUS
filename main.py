d754e18c5042a398b1dfa94a12f1be3916f773b7
Sha2020-ship-it <72983931+Sha2020-ship-it@users.noreply.github.com>
2021-02-20 17:12:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,23 @@ pygame.init()
 WIDTH, HEIGHT = 720, 400
 color = (255, 255, 255)
 color_light = (170, 170, 170)
+
 color_dark = (100, 100, 100)
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("CTRL+ALT+FOCUS")
+
 Font = pygame.font.SysFont('Courier New', 35)
 Font1 = pygame.font.SysFont('Courier New', 15)
 Font2 = pygame.font.SysFont('Courier New', 20)
+
 Text = Font.render('Go to Smile Detctor!' , True , color)
 text1 = Font.render("CTRL+ALT+FOCUS", True, color)
 text2 = Font.render('Go to Smile', True, color)
+
 text3 = Font.render('DETECTOR!', True, color)
 text4 = Font.render('Go to Eye', True, color)
 text5 = Font.render('DETECTOR!', True, color)
+
 face_detector = cv2.CascadeClassifier('C:\\Users\\sharv\\PycharmProjects\\OPENCV\\haarcascade_frontalface_default.xml')
 smile_detector = cv2.CascadeClassifier('C:\\Users\\sharv\\PycharmProjects\\OPENCV\\haarcascade_smile.xml')
 eye_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'C:\\Users\\sharv\\PycharmProjects\\OPENCV\\haarascade_eye.xml')
@@ -30,6 +35,7 @@ eye_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'C:\\Users\\sharv\\
 def smile():
     global RED_TIMES, GREEN_TIMES
     # Password for the email and saving the password in a database
+    
     RED_TIMES = 0
     GREEN_TIMES = 0
     count = 0
@@ -63,6 +69,7 @@ def smile():
         # Change to grayscale
         frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # Detect faces first
+        
         faces = face_detector.detectMultiScale(frame_grayscale)
         # Detect smiles
         # Run smile detection within each of those faces
@@ -90,13 +97,17 @@ def smile():
             message = f"""No. of times you were focused: {GREEN_TIMES // 5}. No. of times you were distracted {RED_TIMES // 5}."""
             message1 = f'Percentage of focus = {round(((GREEN_TIMES / (GREEN_TIMES + RED_TIMES)) * 100))}%'
             text6 = Font1.render(message, True, color_light)
+            
             if round(((GREEN_TIMES / (GREEN_TIMES + RED_TIMES)) * 100)) in range(-1, 40):
                 text7 = Font1.render(message1, True, (255, 0, 0))
+            
             elif round(((GREEN_TIMES / (GREEN_TIMES + RED_TIMES)) * 100)) in range(39, 70):
                 text7 = Font1.render(message1, True, (255, 255, 0))
+            
             elif round(((GREEN_TIMES / (GREEN_TIMES + RED_TIMES)) * 100)) in range(69, 100):
                 text7 = Font1.render(message1, True, (0, 255, 0))
             text8 = Font1.render(state, True, color_light)
+            
             SCREEN.blit(text6, (10, 180))
             SCREEN.blit(text7, (10, 210))
             SCREEN.blit(text8, (10, 240))
@@ -107,6 +118,7 @@ def smile():
             the_face = frame[y:y + h, x:x + w]
             # the_face = (x, y, w, h)
             # Change to grayscale
+            
             face_grayscale = cv2.cvtColor(the_face, cv2.COLOR_BGR2GRAY)
             smiles = smile_detector.detectMultiScale(face_grayscale, scaleFactor=1.7, minNeighbors=20)
 
@@ -115,6 +127,7 @@ def smile():
             if len(smiles) > 0:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
                 RED_TIMES += 1
+            
             else:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 GREEN_TIMES += 1
@@ -184,12 +197,15 @@ def eye():
             shape = predictor(gray, rect)
             shape = shape_to_np(shape)
             mask = np.zeros(img.shape[:2], dtype=np.uint8)
+            
             mask = eye_on_mask(mask, left)
             mask = eye_on_mask(mask, right)
             mask = cv2.dilate(mask, kernel, 5)
+            
             eyes = cv2.bitwise_and(img, img, mask=mask)
             mask = (eyes == [0, 0, 0]).all(axis=2)
             eyes[mask] = [255, 255, 255]
+            
             mid = (shape[42][0] + shape[39][0]) // 2
             mid_vals.append(mid)
             for n in range(len(mid_vals)):
@@ -201,12 +217,15 @@ def eye():
             eyes_gray = cv2.cvtColor(eyes, cv2.COLOR_BGR2GRAY)
             threshold = cv2.getTrackbarPos('threshold', 'image')
             _, thresh = cv2.threshold(eyes_gray, threshold, 255, cv2.THRESH_BINARY)
+            
             thresh = cv2.erode(thresh, None, iterations=2)  # 1
             thresh = cv2.dilate(thresh, None, iterations=4)  # 2
             thresh = cv2.medianBlur(thresh, 3)  # 3
+            
             thresh = cv2.bitwise_not(thresh)
             contouring(thresh[:, 0:mid], mid, img, False)
             contouring(thresh[:, mid:], mid, img, True)
+            
             # for (x, y) in shape[36:48]:
             #     cv2.circle(img, (x, y), 2, (255, 0, 0), -1)
         # show the image with the face detections + facial landmarks
@@ -239,12 +258,16 @@ while True:
     MOUSE = pygame.mouse.get_pos()
     pygame.draw.rect(SCREEN, (255, 0, 0), [0, 70, WIDTH/2, 100])
     pygame.draw.rect(SCREEN, (0, 255, 0), [WIDTH/2, 70, WIDTH, 100])
+    
     pygame.draw.line(SCREEN, (255, 255, 255), (0, 280), (WIDTH, 280))
     SCREEN.blit(text1, (25, 25))
+    
     SCREEN.blit(text2, (50, 80))
     SCREEN.blit(text3, (85, 110))
+    
     SCREEN.blit(text4, (430, 80))
     SCREEN.blit(text5, (430, 110))
+    
     pygame.display.update()
 
 cv2.destroyAllWindows()
